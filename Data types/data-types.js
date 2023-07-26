241510c65f5211f819977f267d9a9406625bdd2f
tinganjoseph <tinganjoseph7@gmail.com>
2023-07-26 17:08:35 +0000

    data type: list the types of data

---
+++ Data types/data-types.js
@@ -0,0 +1,10 @@
+/**
+ * !Javascript has six primitive data types:
+ * *1. null
+ * *2. undefined
+ * *3. boolean
+ * *4. number
+ * *5. string
+ * *6. symbol-available only from ES6
+ * ?and one comple data type is called object
+ */
