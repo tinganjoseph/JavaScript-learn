ce18f2ec4429fc19bf7f037029c793da1b6a3e11
tinganjoseph <tinganjoseph7@gmail.com>
2023-08-11 06:19:52 +0000

    Operators: Unary Operators explanations

---
+++ 2. Operators/1.Unary Operators/Unary.operators.js
@@ -0,0 +1,9 @@
+/**
+ * *The simplest operators in JavaScript are unary operators. A unary operator works on one operand. The unary operators in JavaScript are: 
+ * !1. Unary plus(+) - convert an operand into a number
+ * !2. Unary minus(-) - convert an operand into a number and negate the value after that.
+ * !3. prefix/ postfix increments(++) - add one to its operand.
+ * !4. prefix/ postfix decrements(--) - substract one from its operand.
+ * 
+ * todo: Note: an operand is any object capable of being manipulated. For example, in "1 + 2" the "1" and "2" are the operands
+ */
